4c2cc0fc2c6a81d779a66ceeec4df8058e0f2c5c
unknown <3fredclark3@gmail.com>
2021-05-11 18:01:22 -0600

    updated timer to Duration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Duration from 'luxon/src/duration';
 
 function App() {
+  const [timerLength, setTimerLength] = useState(5);
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerFinish, setTimerFinish] = useState(true);
+  const [sessionType, setSessionType] = useState('Work');
+  const [sessionNumber, setSessionNumber] = useState(0);
+
+  //useContext for long break length
+  //useContext for break length
+  //useContext for worklength
+
+  //useEffect1 [timerOn]
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerOn) {
+        setTimerLength((timerLength) => {
+          return timerLength - 1;
+        });
+      }
+    }, 1000);
+    if (timerOn) {
+      setTimerFinish(false);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerOn]);
+
+  //useEffect2 [timerLength]
+  useEffect(() => {
+    if (timerLength === 0) {
+      setTimerFinish(true);
+      setTimerOn(false);
+      setSessionType((pastType) => {
+        if (pastType === 'Work') {
+          setSessionType('Break');
+        }
+        if (pastType === 'Break') {
+          setSessionType('Work');
+        }
+        if (pastType === 'LongBreak') {
+          setSessionType('Work');
+        }
+      });
+    }
+  }, [timerLength]);
+
+  //useEffect3-5 [sesssionType, break/work/longbreak]
+  useEffect(() => {
+    if (timerFinish === true) {
+      if (sessionType === 'Break') {
+        setTimerLength(5);
+      }
+      if (sessionType === 'Work') {
+        setTimerLength(3);
+      }
+      if (sessionType === 'LongBreak') {
+        setTimerLength(15);
+      }
+    }
+  }, [sessionType]);
+  //useEffect6 sound [sessionType, timerDone]
+  useEffect(() => {
+    if (!timerFinish) {
+      if (sessionNumber === 1) {
+        setTimerLength(3);
+        setSessionType('Work');
+      }
+      if (sessionNumber === 2) {
+        setTimerLength(5);
+        setSessionType('Break');
+      }
+    }
+    if (!timerOn) {
+      setSessionNumber(sessionNumber + 1);
+    }
+  }, [sessionType, timerFinish]);
+
+  //useEffect 7 [sessionNumber]
+  useEffect(() => {
+    if (sessionNumber > 5) {
+      setSessionType('LongBreak');
+      setSessionNumber(0 - 1);
+    }
+  }, [sessionNumber]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        Duration:
+        {Duration.fromObject({ seconds: timerLength }).toFormat('mm:ss')}
+      </p>
+      <button onClick={() => setTimerOn(!timerOn)}>
+        {timerOn ? 'Pause' : 'Run'}
+      </button>
+      {/* {console.log(sessionType)} */}
+      <p>{timerFinish ? 'Timer is completed' : 'Timer running'}</p>
+      <p>SessionType:{sessionType}</p>
+      <p>Session Number: {sessionNumber}</p>
+    </>
   );
 }
 
